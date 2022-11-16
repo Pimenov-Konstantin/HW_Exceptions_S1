40aef08a757a8ccdfe399498340adac7c25f851a
KonPim <100049831+Pimenov-Konstantin@users.noreply.github.com>
2022-11-17 01:02:30 +0300

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+/*
+Посмотрите на код, и подумайте сколько разных типов исключений вы тут сможете получить?
+for (int j = 0; j < 5; j++) - выкинет ArrayIndexOutOfBoundsException, если массив больше 5.
+int val = Integer.parseInt(arr[i][j]) - выкинет NumberFormatException, если строка не число.
+arr.length - выкинет NullPointerException, если входящий массив arr указывает на пустую ссылку.
+*/
+}
